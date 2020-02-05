1e5ff8e57687b449fd5b8fe815bde6e8c349a8b8
Rinat <rinat@Sergeys-Mac-mini.local>
2020-02-05 20:05:27 +0500

    first try generate

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+const debug = require('debug')('combinator');
+
 const { sequelize } = require('./models');
 const bip39 = require('bip39');
 
 const { fact, permutation } = require('./utils/combinator');
 
 async function combinator() {
-  const array = bip39.generateMnemonic().split(' ');
+  debug('run combinator');
 
-  for (let i=0; i < fact(array.length); i++) {
+  const array = bip39.generateMnemonic().split(' ');
+  const length = fact(array.length)
+
+  for (let i=0; i < length; i++) {
+    const phrase = permutation(i, array.slice(0)).join(' ');
+
     await sequelize.models.Word.create({
-      phrase: permutation(i, array.slice(0)).join(' ')
-    })
+      phrase
+    });
+
+    debug('generated', i, 'of', length, 'phrase:', phrase);
   }
 }
 
