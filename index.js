c0c4aa52a8008b659153c4bac6c7a183edeae3fe
Rinat <lich666black@gmail.com>
2020-04-10 16:45:11 +0000

    fixed

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-const debug = require('debug')('combinator');
-
-const { sequelize } = require('./models');
-const bip39 = require('bip39');
+// const { sequelize } = require('./models');
+const { Zilliqa } = require('@zilliqa-js/zilliqa');
+// const bip39 = require('bip39');
 
 const { fact, permutation } = require('./utils/combinator');
 
 async function combinator() {
-  debug('run combinator');
+  const needFind = 'paste your address';
+  const seed = 'paste your seed phrase';
 
-  const array = bip39.generateMnemonic().split(' ');
-  const length = fact(array.length)
+  const array = seed.split(' ');
+  const length = fact(array.length);
 
-  for (let i=0; i < length; i++) {
+  for (let i = 0; i < length; i++) {
+    const zilliqa = new Zilliqa('https://dev-api.zilliqa.com');
     const phrase = permutation(i, array.slice(0)).join(' ');
 
-    await sequelize.models.Word.create({
-      phrase
-    });
+    try {
+      if (!zilliqa.wallet.isValidMnemonic(phrase)) {
+        continue;
+      }
 
-    debug('generated', i, 'of', length, 'phrase:', phrase);
+      zilliqa.wallet.addByMnemonic(phrase);
+
+      const { bech32Address } = zilliqa.wallet.defaultAccount;
+
+      if (needFind == bech32Address) {
+        console.log('FOUND', phrase);
+
+        return null;
+      }
+
+      console.log('generated', i, 'of', length, 'phrase:', phrase, bech32Address);
+    } catch (err) {
+      console.log('generated', i, 'of', length, 'skip');
+    }
   }
 }
 
